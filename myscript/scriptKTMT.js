24fab3ff36f53ba74fd1c2e53a8f6db854acb4e5
= <=>
2021-05-13 18:32:42 +0700

    update nav 18h32 13/05/2021

--- myscript/scriptKTMT.js
+++ myscript/scriptKTMT.js
@@ -32,22 +32,7 @@ function scrollFunction() {
 	}
 }
 
-		var mybutton = document.getElementById("myBtn");
-
-		window.onscroll = function() {scrollFunction()};
-
-		function scrollFunction() {
-			if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-				mybutton.style.display = "block";
-			} else {
-				mybutton.style.display = "none";
-			}
-		}
-
-
-
-
-
+		
 
 
 function topFunction() {
