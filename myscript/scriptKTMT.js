ed026705717e31127424d4f6cb926a0fe2ca32ee
vinhuy2002 <83992296+vinhuy2002@users.noreply.github.com>
2021-05-31 09:07:09 +0700

    SmallUpdate
    SmallUpdate, I don't even know who I am

--- myscript/scriptKTMT.js
+++ myscript/scriptKTMT.js
@@ -76,6 +76,22 @@ function showSlides(n) {
 }
 
 
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50 ) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
+});
+$(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$(".thetop").offset().top},"1000");return false})})
+
+
+$(document).ready(function(){
+  $('.toast').toast('show');
+});
+
+
+
 
 
 
