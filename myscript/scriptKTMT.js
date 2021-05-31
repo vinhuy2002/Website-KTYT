00c8d0bc59e2a289afa3c0654bc373f99aa26516
vinhuy2002 <83992296+vinhuy2002@users.noreply.github.com>
2021-05-31 07:50:22 +0700

    Small changes
    Small change, I don't know who even I am anymore

--- myscript/scriptKTMT.js
+++ myscript/scriptKTMT.js
@@ -7,7 +7,7 @@ function filterFunction() {
   var input, filter, ul, li, a, i;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
+  div = document.getElementById("navDrop1");
   a = div.getElementsByTagName("a");
   for (i = 0; i < a.length; i++) {
     txtValue = a[i].textContent || a[i].innerText;
@@ -31,6 +31,9 @@ function scrollFunction() {
 		mybutton.style.display = "none";
 	}
 }
+
+		
+
 
 function topFunction() {
 	document.body.scrollTop = 0;
@@ -71,3 +74,247 @@ function showSlides(n) {
 	slides[slideIndex-1].style.display = "block";  
 	dots[slideIndex-1].className += " active";
 }
+
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50 ) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
+});
+$(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$(".thetop").offset().top},"1000");return false})})
+
+
+$(document).ready(function(){
+  $('.toast').toast('show');
+});
+
+
+
+
+
+
+/* Demo purposes only */
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+);
+
+
+
+function myNavKhac(x) {
+  x.classList.toggle("change");
+}
+
+
+// external js: isotope.pkgd.js
+var $grid = $('.grid-container').isotope({
+  itemSelector: '.grid-item'
+});
+
+// store filter for each group
+var filters = {};
+
+$('.filters').on( 'click', '.button', function( event ) {
+  var $button = $( event.currentTarget );
+  // get group key
+  var $buttonGroup = $button.parents('.button-group');
+  var filterGroup = $buttonGroup.attr('data-filter-group');
+  // set filter for group
+  filters[ filterGroup ] = $button.attr('data-filter');
+  // combine filters
+  var filterValue = concatValues( filters );
+  // set filter for Isotope
+  $grid.isotope({ filter: filterValue });
+});
+
+
+// change is-checked class on buttons
+$('.scroll-ul').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'li', function( event ) {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    var $button = $( event.currentTarget );
+    $button.addClass('is-checked');
+  });
+});
+  
+  
+// flatten object by concatting values
+function concatValues( obj ) {
+  var value = '';
+  for ( var prop in obj ) {
+    value += obj[ prop ];
+  }
+  return value;
+}
+
+function getHashFilter() {
+  // get filter=filterName
+  var matches = location.hash.match( /filter=([^&]+)/i );
+  var hashFilter = matches && matches[1];
+  return hashFilter && decodeURIComponent( hashFilter );
+}
+
+// init Isotope
+var $grid = $('.grid-container');
+
+// bind filter button click
+var $filterButtonGroup = $('.filter-button-group');
+$filterButtonGroup.on( 'click', 'li', function() {
+  var filterAttr = $( this ).attr('data-filter');
+  // set filter in hash
+  location.hash = 'filter=' + encodeURIComponent( filterAttr );
+});
+
+var isIsotopeInit = false;
+
+function onHashchange() {
+  var hashFilter = getHashFilter();
+  if ( !hashFilter && isIsotopeInit ) {
+    return;
+  }
+  isIsotopeInit = true;
+  // filter isotope
+  $grid.isotope({
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows',
+    // use filterFns
+    filter: filters[ hashFilter ] || hashFilter
+  });
+  // set selected class on button
+  if ( hashFilter ) {
+    $filterButtonGroup.find('.is-checked').removeClass('is-checked')  + window.scrollTo(0, 0);
+    $filterButtonGroup.find('[data-filter="' + hashFilter + '"]').addClass('is-checked') ;
+	 window
+  }
+}
+
+$(window).on( 'hashchange', onHashchange );
+
+// trigger event handler to init Isotope
+onHashchange();
+
+
+
+
+
+// external js: isotope.pkgd.js
+var $grid = $('.grid-container').isotope({
+  itemSelector: '.grid-item'
+});
+
+// store filter for each group
+var filters = {};
+
+$('.filters').on( 'click', '.button', function( event ) {
+  var $button = $( event.currentTarget );
+  // get group key
+  var $buttonGroup = $button.parents('.button-group');
+  var filterGroup = $buttonGroup.attr('data-filter-group');
+  // set filter for group
+  filters[ filterGroup ] = $button.attr('data-filter');
+  // combine filters
+  var filterValue = concatValues( filters );
+  // set filter for Isotope
+  $grid.isotope({ filter: filterValue });
+});
+
+
+// change is-checked class on buttons
+$('.scroll-ul').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'li', function( event ) {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    var $button = $( event.currentTarget );
+    $button.addClass('is-checked');
+  });
+});
+  
+  
+// flatten object by concatting values
+function concatValues( obj ) {
+  var value = '';
+  for ( var prop in obj ) {
+    value += obj[ prop ];
+  }
+  return value;
+}
+
+function getHashFilter() {
+  // get filter=filterName
+  var matches = location.hash.match( /filter=([^&]+)/i );
+  var hashFilter = matches && matches[1];
+  return hashFilter && decodeURIComponent( hashFilter );
+}
+
+// init Isotope
+var $grid = $('.grid-container');
+
+// bind filter button click
+var $filterButtonGroup = $('.filter-button-group');
+$filterButtonGroup.on( 'click', 'li', function() {
+  var filterAttr = $( this ).attr('data-filter');
+  // set filter in hash
+  location.hash = 'filter=' + encodeURIComponent( filterAttr );
+});
+
+var isIsotopeInit = false;
+
+function onHashchange() {
+  var hashFilter = getHashFilter();
+  if ( !hashFilter && isIsotopeInit ) {
+    return;
+  }
+  isIsotopeInit = true;
+  // filter isotope
+  $grid.isotope({
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows',
+    // use filterFns
+    filter: filters[ hashFilter ] || hashFilter
+  });
+  // set selected class on button
+  if ( hashFilter ) {
+    $filterButtonGroup.find('.is-checked').removeClass('is-checked')  + window.scrollTo(0, 0);
+    $filterButtonGroup.find('[data-filter="' + hashFilter + '"]').addClass('is-checked') ;
+   window
+  }
+}
+
+$(window).on( 'hashchange', onHashchange );
+
+// trigger event handler to init Isotope
+onHashchange();
+
+
+
+
+
+
+window.onscroll = function() {myFunction()};
+// Get the header
+var header = document.getElementById("myBtnContainer");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+  
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  header.style.top = ("0" + "px")
+  header.style.marginLeft = ("0" + "px")
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+  
+
+
+	
+
